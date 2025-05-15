cae1917b8df0a882d7811f9e72b224eb536d991c
NikoDani <jokhadzenikolozi22@gmail.com>
2025-05-15 21:31:38 +0400

    Initial commit

---
+++ დავალება/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">სახელი: ნიკა</h1>
+      <h1 className="title">გვარი: კვეკვესკირი</h1>
+      <h1 className="title">ასაკი: 20</h1>
+      <h1 className="title">ჰობი: პროგრამირება და ფეხბურთი</h1>
+    </div>
+  );
+}
+
+export default App;
