1982858aff3be6af241b4b444895e91cbd571397
NikoDani <jokhadzenikolozi22@gmail.com>
2025-05-15 21:54:48 +0400

    Updated App.jsx with new content

--- დავალება/src/App.jsx
+++ დავალება/src/App.jsx
@@ -1,12 +1,13 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
-      <h1 className="title">სახელი: ნიკა</h1>
-      <h1 className="title">გვარი: კვეკვესკირი</h1>
-      <h1 className="title">ასაკი: 20</h1>
-      <h1 className="title">ჰობი: პროგრამირება და ფეხბურთი</h1>
+      <h1 className="title">სახელი: ნიკოლოზ</h1>
+      <h1 className="title">გვარი: ჯოხაძე</h1>
+      <h1 className="title">ასაკი: 15</h1>
+      <h1 className="title">ჰობი: წყალბურთი, კოდინგი და ფეხბურთი</h1>
     </div>
   );
 }
